b2a678142df42fadc1a7996a8ef574068ea3fdbf
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2022-02-23 18:27:41 -0700

    Write code for number and clear buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let displayValue = "0";
+let secondValue = "0";
+let operator = null;
+
 function add(x, y) {
     return x + y;
 }
@@ -17,3 +21,30 @@ function divide(x, y) {
 function operate(x, y, operator) {
     return operator(x, y);
 }
+
+function clickOperand(e) {
+    if (displayValue === "0") {
+        displayValue = this.value;
+    } else {
+        displayValue += this.value;
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    const display = document.querySelector('#display');
+    display.textContent = displayValue;
+}
+
+function clear(e) {
+    displayValue = "0";
+    secondValue = "0";
+    operator = null;
+    updateDisplay();
+}
+
+const operandButtons = document.querySelectorAll('.operand');
+operandButtons.forEach(button => button.addEventListener('click', clickOperand));
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clear);
+updateDisplay();
