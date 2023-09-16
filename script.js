b3fff744d67e82ec7ea4088b1b338b98d3e18b92
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2023-09-16 16:47:48 -0700

    Fix decimal button when building new operand

--- script.js
+++ script.js
@@ -42,7 +42,14 @@ function clickDecimal(e) {
         // if we already have a decimal point, then we disable the button.
         return;
     }
-    displayValue += this.value;
+    if (operand === null) {
+        // if building a new operand, add a zero before the decimal point
+        displayValue = "0.";
+        // and set the operand to not be null
+        operand = 0;
+    } else {
+        displayValue += this.value;
+    }
     updateDisplay();
 }
 
